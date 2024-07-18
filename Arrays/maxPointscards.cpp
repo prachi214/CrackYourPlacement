1e373a86b8aa240faf11e6a9d322641e66101e0d
Prachi Patel <prachipatel5441@gmail.com>
2024-07-18 19:13:01 +0530

    Maximum Points You Can Obtain from Cards

---
+++ Arrays/maxPointscards.cpp
@@ -0,0 +1,25 @@
+//Maximum Points You Can Obtain from Cards
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+
+       int leftSum = 0, rightSum = 0, maxSum = 0;
+    //    left part
+       for(int i=0;i<k;i++) {
+leftSum+= cardPoints[i];
+       }
+       maxSum = leftSum;
+      int  rightIndex = cardPoints.size() - 1;
+    //    right part
+
+       for(int i = k-1;i>=0;i--){
+        leftSum-= cardPoints[i];
+        rightSum+= cardPoints[rightIndex];
+        rightIndex--;
+        maxSum= max(maxSum, leftSum + rightSum);
+       }
+       
+return maxSum;
+    }
+};
