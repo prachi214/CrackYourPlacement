f815d9b8cf0fa828d9a3fdcddf0019ad7cd6d14d
Prachi Patel <prachipatel5441@gmail.com>
2024-07-24 02:48:45 +0530

    correction in code

--- LinkedList/reverseLLKgroups.cpp
+++ LinkedList/reverseLLKgroups.cpp
@@ -1,37 +1,43 @@
 // Reverse Nodes in k-Group
 
-class Solution
-{
+class Solution {
 public:
-    ListNode *reverseKGroup(ListNode *head, int k)
-    {
+
+int findLength(ListNode* &head) {
+  ListNode* temp = head;
+  int len = 0;
+  while(temp != NULL) {
+    len++;
+    temp = temp->next;
+  }
+  return len;
+}
+    ListNode* reverseKGroup(ListNode* head, int k) {
         // base case
-        if (head == NULL)
-            return head;
-        if (head->next == NULL)
-            return head;
-
+        if(head == NULL) return head;
+        // SINGLE NODE
+        if(head->next == NULL) return head;
         // reverse 1st k node
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        ListNode *nextNode = curr->next;
-
+      ListNode* prev = NULL;
+           ListNode* curr = head;
+        ListNode* nextNode = curr->next;
         int pos = 0;
-        while (pos < k)
-        {
-            pos++;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-            pos++;
+        int len = findLength(head);
+        // check kth node present or not
+        if(len<k) return head;
+        while(pos<k){
+            nextNode = curr->next;
+                    curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+        pos++;
         }
 
-        ListNode *recursionAns = NULL;
-        if (nextNode != NULL)
-        {
-            recursionAns = reverseKGroup(nextNode, k);
-            head->next = recursionAns;
-        }
-        return prev;
+ListNode* recursionAns = NULL;
+if(nextNode != NULL){
+    recursionAns = reverseKGroup(nextNode, k);
+    head->next = recursionAns;
+}
+return prev;
     }
 };
