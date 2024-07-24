bbd412cd6479c0ac3dd0abddec77a23d1d27d892
Prachi Patel <prachipatel5441@gmail.com>
2024-07-24 21:58:55 +0530

    Merge 2 Sort Linked list

---
+++ LinkedList/merge2SortLL.cpp
@@ -0,0 +1,38 @@
+// Merge Two Sorted Lists
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (list1 == 0)
+            return list2;
+        if (list2 == 0)
+            return list1;
+
+        ListNode *ans = new ListNode(-1);
+        ListNode *mptr = ans;
+
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
+                mptr->next = list1;
+                mptr = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                mptr->next = list2;
+                mptr = list2;
+                list2 = list2->next;
+            }
+        }
+        if (list1)
+            mptr->next = list1;
+        if (list2)
+            mptr->next = list2;
+
+        return ans->next;
+    }
+};
