78fdc5b0419d4a90c9690f3862cbe99d45578ae1
Prachi Patel <prachipatel5441@gmail.com>
2024-08-03 21:52:23 +0530

    Permute 2 array for sum K

---
+++ SortingandSearching/Permute2arrsumK.cpp
@@ -0,0 +1,19 @@
+//Permute two arrays such that sum of every pair is greater or equal to K
+
+class Solution {
+  public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        sort(arr1.begin(), arr1.end());
+        // sort eles in descending order
+          sort(arr2.begin(), arr2.end(),greater<int>());
+          bool flag = false;
+          int n = arr1.size();
+          
+        for(int i = 0; i < n; i++) {
+            if(arr1[i] + arr2[i] < k) {
+                return false;  
+            }
+        }
+        return true; 
+        }
+};
